c3877d34d31160798066c0e3b07749a42db819f7
Achintya Jai <153343775+pUrGe12@users.noreply.github.com>
2024-05-31 12:49:35 +0530

    Update AES.py

--- TestFiles_/Encryption_Decryption/AES.py
+++ TestFiles_/Encryption_Decryption/AES.py
@@ -1,1 +1,15 @@
-a
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad, unpad
+
+data = b'''
+<keylogger raw CPP code>
+'''
+
+key = b'<prededfined 16 byte key>'
+
+cipher = AES.new(key, AES.MODE_ECB)
+data = pad(data, AES.block_size)
+ciphertext = cipher.encrypt(data)
+
+with open("DIFX.txt", 'wb') as fp:
+	fp.write(ciphertext)
