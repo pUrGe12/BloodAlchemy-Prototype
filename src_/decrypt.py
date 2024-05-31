71e59bef48076cebba7f2b841cc2ced06d66d055
Achintya Jai <153343775+pUrGe12@users.noreply.github.com>
2024-05-31 19:22:32 +0530

    Create decrypt.py

---
+++ src_/decrypt.py
@@ -0,0 +1,11 @@
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad, unpad
+
+key = b'<predefined key>'
+with open('DIFX.txt', 'rb') as fp:
+	encrypted = fp.read()
+decipher = AES.new(key, AES.MODE_ECB)
+data_back = decipher.decrypt(encrypted)
+
+with open("decoded_output.cpp", 'w') as bp:
+	bp.write(data_back.decode())
