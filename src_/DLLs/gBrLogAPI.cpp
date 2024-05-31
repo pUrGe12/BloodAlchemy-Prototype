16f9d63d0e496d3f52d8f58a7f4afbee8a1a8c26
Achintya Jai <153343775+pUrGe12@users.noreply.github.com>
2024-05-31 12:18:57 +0530

    Create gBrLogAPI.cpp

---
+++ src_/DLLs/gBrLogAPI.cpp
@@ -0,0 +1,14 @@
+#include <windows.h>
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved) {
+    switch (ul_reason_for_call){
+    case DLL_PROCESS_ATTACH:
+        MessageBoxA(NULL, "SUCCESS", "Notification", MB_OK | MB_ICONINFORMATION);
+        break;
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
